c538358b16b6afde920d17ae267a51c53b24b7a1
Pedro <pdanielaraujo@ipvc.pt>
2021-03-07 00:03:05 +0000

    Primeiro Commit
    Algum avanço no layout de Login, criação do BottomNavigationBar com fragmentos já funcional para trocar entre eles. Alguma estruturação primária.

--- app/src/main/java/pt/atp/cityalert/MainActivity.kt
+++ app/src/main/java/pt/atp/cityalert/MainActivity.kt
@@ -1,11 +1,51 @@
 package pt.atp.cityalert
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import pt.atp.cityalert.fragments.HomeFragment
+import pt.atp.cityalert.fragments.NotesFragment
+import pt.atp.cityalert.fragments.PerfilFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val home_fragment = HomeFragment()
+        val notes_fragment = NotesFragment()
+        val perfil_fragment = PerfilFragment()
+
+        setMainFragment(home_fragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> setMainFragment(home_fragment)
+                R.id.notes -> setMainFragment(notes_fragment)
+                R.id.person -> setMainFragment(perfil_fragment)
+            }
+            true
+        }
+
+    }
+
+    private fun setMainFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.layout_fragment, fragment)
+            commit()
+        }
+
+    fun btn(view: View){
+        val intent = Intent(this, LoginActivity::class.java).apply {
+
+        }
+        startActivity(intent)
     }
 }
